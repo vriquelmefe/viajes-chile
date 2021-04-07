9912433a9a87171bb9c43efcb80f4a0787a916d3
Vanessa_Riquelme <vriquelmefe@gmail.com>
2021-04-07 02:57:48 -0400

    modifico formulario, header le doi estilo y aplico media query,section nosotros le doi estilo a titulo, se le agrega estilo de fuentes al body

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 $(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
 
+            var idScroll = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(idScroll).offset().top
+            }, 1200, function() {
+                window.location.hash = idScroll;
+            });
+        }
+    });
 });
